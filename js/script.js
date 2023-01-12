dce95ef7c3802d15f83354d848a863be4e6a9eb3
mostafa2113 <ace13520@gmail.com>
2023-01-12 18:20:39 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const joke = document.querySelector("#joke");
+const jokeText = document.querySelector(".joke-text");
+const jokecontainer = document.querySelector(".joke-container");
+const newJoke = document.querySelector("#new-joke");
+const twitter = document.querySelector("#twitter");
+const loader = document.querySelector(".loader");
+//loader
+function loading(loaded) {
+    loader.style.display = "block";
+    jokecontainer.style.display = "none";
+    if (loaded) {
+        getJoke()
+    }
+}
+//get joke from api
+async function getJoke() {
+    const apiUrl = "https://v2.jokeapi.dev/joke/Programming?type=single"
+    try {
+        const data = await (await fetch(apiUrl)).json();
+        if (data.joke.length > 12) {
+            jokeText.classList.add("long-joke");
+        } else {
+            jokeText.classList.remove("long-joke");
+        }
+        loader.style.display = "none";
+        jokecontainer.style.display = "block";
+        joke.innerHTML = data.joke;
+    } catch (error) {
+        console.log("no jokes", error);
+    }
+}
+function twitte() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${joke.innerHTML}`;
+    window.open(twitterUrl, '_blank');
+}
+//onloading
+loading(false);
+//loaded
+document.addEventListener("DOMContentLoaded", () => {
+    getJoke();
+    loader.style.display = "none";
+    jokecontainer.style.display = "block";
+
+    newJoke.addEventListener("click", () => loading(true));
+    twitter.addEventListener("click", twitte);
+})
